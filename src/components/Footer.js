f153a830fd0b1322a891fb9959615e4ead105d90
EZDevanshu <devanshuparmar00@gmail.com>
2026-01-10 17:16:25 +0530

    changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import "../css/Footer.css";
 
 function Footer() {
@@ -10,38 +9,38 @@ function Footer() {
                         <div className="row">
                             <div className="col-lg-2 col-md-4 col-sm-6 mb-4">
                                 <h6 className="footer-heading">About</h6>
-                                <a href="#" className="footer-link">Contact Us</a>
-                                <a href="#" className="footer-link">About Us</a>
-                                <a href="#" className="footer-link">Careers</a>
-                                <a href="#" className="footer-link">Flipkart Stories</a>
-                                <a href="#" className="footer-link">Press</a>
-                                <a href="#" className="footer-link">Corporate Information</a>
+                                <a href="/" className="footer-link">Contact Us</a>
+                                <a href="/" className="footer-link">Careers</a>
+                                <a href="/" className="footer-link">Flipkart Stories</a>
+                                <a href="/" className="footer-link">Press</a>
+                                <a href="/" className="footer-link">About Us</a>
+                                <a href="/" className="footer-link">Corporate Information</a>
                             </div>
 
                             <div className="col-lg-2 col-md-4 col-sm-6 mb-4">
                                 <h6 className="footer-heading">Group Companies</h6>
-                                <a href="#" className="footer-link">Myntra</a>
-                                <a href="#" className="footer-link">Cleartrip</a>
-                                <a href="#" className="footer-link">Shopsy</a>
+                                <a href="/" className="footer-link">Myntra</a>
+                                <a href="/" className="footer-link">Cleartrip</a>
+                                <a href="/" className="footer-link">Shopsy</a>
                             </div>
 
                             <div className="col-lg-2 col-md-4 col-sm-6 mb-4">
                                 <h6 className="footer-heading">Help</h6>
-                                <a href="#" className="footer-link">Payments</a>
-                                <a href="#" className="footer-link">Shipping</a>
-                                <a href="#" className="footer-link">Cancellation & Returns</a>
-                                <a href="#" className="footer-link">FAQ</a>
+                                <a href="/" className="footer-link">Payments</a>
+                                <a href="/" className="footer-link">Shipping</a>
+                                <a href="/" className="footer-link">Cancellation & Returns</a>
+                                <a href="/" className="footer-link">FAQ</a>
                             </div>
 
                             <div className="col-lg-2 col-md-4 col-sm-6 mb-4">
                                 <h6 className="footer-heading">Consumer Policy</h6>
-                                <a href="#" className="footer-link">Cancellation & Returns</a>
-                                <a href="#" className="footer-link">Terms Of Use</a>
-                                <a href="#" className="footer-link">Security</a>
-                                <a href="#" className="footer-link">Privacy</a>
-                                <a href="#" className="footer-link">Sitemap</a>
-                                <a href="#" className="footer-link">Grievance Redressal</a>
-                                <a href="#" className="footer-link">EPR Compliance</a>
+                                <a href="/" className="footer-link">Cancellation & Returns</a>
+                                <a href="/" className="footer-link">Terms Of Use</a>
+                                <a href="/" className="footer-link">Security</a>
+                                <a href="/" className="footer-link">Privacy</a>
+                                <a href="/" className="footer-link">Sitemap</a>
+                                <a href="/" className="footer-link">Grievance Redressal</a>
+                                <a href="/" className="footer-link">EPR Compliance</a>
                             </div>
 
                             <div className="col-lg-2 col-md-4 col-sm-6 mb-4">
@@ -57,10 +56,10 @@ function Footer() {
 
                                 <div className="social-icons">
                                     <span style={{color: '#878787', fontSize: '12px', display: 'block', marginBottom: '10px'}}>Social:</span>
-                                    <a href="#"><i className="bi bi-facebook"></i></a>
-                                    <a href="#"><i className="bi bi-twitter-x"></i></a>
-                                    <a href="#"><i className="bi bi-youtube"></i></a>
-                                    <a href="#"><i className="bi bi-instagram"></i></a>
+                                    <a href="/"><i className="bi bi-facebook"></i></a>
+                                    <a href="/"><i className="bi bi-twitter-x"></i></a>
+                                    <a href="/"><i className="bi bi-youtube"></i></a>
+                                    <a href="/"><i className="bi bi-instagram"></i></a>
                                 </div>
                             </div>
 
@@ -87,10 +86,10 @@ function Footer() {
                     <div className="container">
                         <div className="bottom-footer-content">
                             <div className="footer-links-inline">
-                                <a href="#"><i className="fas fa-store"></i> Become a Seller</a>
-                                <a href="#"><i className="fas fa-bullhorn"></i> Advertise</a>
-                                <a href="#"><i className="fas fa-gift"></i> Gift Cards</a>
-                                <a href="#"><i className="fas fa-question-circle"></i> Help Center</a>
+                                <a href="/"><i className="fas fa-store"></i> Become a Seller</a>
+                                <a href="/"><i className="fas fa-bullhorn"></i> Advertise</a>
+                                <a href="/"><i className="fas fa-gift"></i> Gift Cards</a>
+                                <a href="/"><i className="fas fa-question-circle"></i> Help Center</a>
                                 <span className="copyright">© 2007-2025 Flipkart.com</span>
                             </div>
 
